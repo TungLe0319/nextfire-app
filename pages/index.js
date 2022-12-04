01496a1688c2595a2c210516f5eff5a39db9b69e
TungLe0319 <tung.le0319@gmail.com>
2022-12-04 13:24:29 -0700

    tung

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,18 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import React from "react"
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link.js";
+import React from "react";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-  <div className="">
-    
-  </div>
-  )
+    <div className="">
+      <Link prefetch={false} href={{
+        pathname:'/[username]',
+        query:{username: 'tung0319godhand'}
+      }}>
+    Tung Le
+      </Link>
+    </div>
+  );
 }
